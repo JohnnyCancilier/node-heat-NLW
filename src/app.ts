883400eb5367f0af7dbadfe87844d6fde3a111ff
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-10-19 01:08:55 -0300

    att4

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,22 @@ import express from "express";
 import { router } from "./routes";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
 
 const serverHttp = http.createServer(app);
 
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuario conectado no socket ${socket.id}`)
+})
 
 app.use(express.json());
 
