721db7999440fc614fbf2fd63ab52e2ede527e73
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-10-19 19:26:46 -0300

    att6

--- src/controllers/createMessageController.ts
+++ src/controllers/createMessageController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { createMessageServices } from "../services/createMessageService";
+import { createMessageService } from "../services/createMessageService";
 
 
 class createMessageController {
     async handle(request: Request, response: Response) {
         const { message } = request.body;
         const { user_id } = request;
-        const service = new createMessageServices();
+        const service = new createMessageService();
         const result = await service.execute(message, user_id);
 
         return response.json(result);
