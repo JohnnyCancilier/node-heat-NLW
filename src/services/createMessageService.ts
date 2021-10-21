29f0b97be462923d81781353ba25b9924507cfd9
Johnny Cancilier <jhony.cancilier@hotmail.com>
2021-10-21 19:49:46 -0300

    att7

--- src/services/createMessageService.ts
+++ src/services/createMessageService.ts
@@ -15,7 +15,7 @@ class createMessageService {
 
         const infoWS = {
             text: message.text,
-            user_id: message.user_id,
+            id: message.id,
             created_at: message.create_at,
             user: {
                 name: message.user.name,
